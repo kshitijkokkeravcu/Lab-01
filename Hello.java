c68e048836e47f1dff4f687227a4bd5e15ec86ad
Kshitij Kokkera <kokkerak@vcu.edu>
2022-08-23 19:51:54 -0400

    first file

---
+++ Hello.java
@@ -0,0 +1,20 @@
+// Hello.java
+// Kshitij Kokkera
+// 08/23/2022
+// CMSC-255-004-FA2022
+
+
+public class Hello{
+	public static void main(String[] args){
+
+		System.out.println("Kshitij Kokkera");
+		System.out.println("Hello.java");
+		System.out.println("Simple hello world");
+		System.out.println("08/23/2022");
+		System.out.println("CMSC-255-004-FA2022");
+		System.out.println();
+
+
+
+	}
+}
